d4025ea23c14ed155db16656723a82b80e578ebd
udalovvv <udalov.vitalii@gmail.com>
2022-08-25 02:33:25 +0500

    Vitalii Udalov

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+
+
+    private Deth8 deth;
+    @Autowired
+    public Egg6(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth.toString();
     }
 }
--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -8,7 +8,7 @@ public class Wood3 {
 
     private final Rabbit4 rabbit;
 
-    @Autowired
+//    @Autowired
     public Wood3(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
